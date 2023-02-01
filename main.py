0a9c4f9231bb20a49b3da7f4d86b7bcc6cc0bfcc
vinicius persoli <vinperdom@gmail.com>
2023-02-01 15:39:35 -0300

    question solved

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import functions_framework
 
 
-def say(message):
-    print(message)
+def greet(message):
+    print("hello, ", message)
 
 
 @functions_framework.cloud_event
 def my_cloudevent_function(cloud_event):
-    say("hello!")
+    greet(cloud_event.data)
