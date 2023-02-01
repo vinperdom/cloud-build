b9c1d641edac5ebfd3ab05ee4cbcd7798ca62f05
vinicius persoli <vinperdom@gmail.com>
2023-02-01 16:03:22 -0300

    question solved

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import functions_framework
 
 
-def greet(name):
-    print(f"hello, {name}")
-
-
 @functions_framework.cloud_event
 def main(cloud_event):
-    greet(cloud_event.data['name'])
+    print(f"hello, {cloud_event.data['name']}")
